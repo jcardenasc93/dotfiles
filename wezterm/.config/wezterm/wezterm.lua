4ab50d89e64582daaa126a26c65a41ad310b4923
jcardenasc93 <jcardenasc93@gmail.com>
2024-02-05 14:24:53 -0500

    New wezterm theme and add venv selector pyright nvim

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 return {
     font = wezterm.font 'GoMono Nerd Font',
     font_size = 12.0,
-    color_scheme = 'nord',
+    color_scheme = 'Catppuccin Mocha',
     window_background_opacity = 0.8,
     hide_tab_bar_if_only_one_tab = true,
     enable_kitty_graphics = true,
